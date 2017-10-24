21e3ec6c43dd2e8717a0a22eb566b489c21ce106
Reginaldo Barros de Sousa <32406036+revelarock@users.noreply.github.com>
2017-10-24 01:33:54 -0200

    Initial commit

---
+++ app/src/androidTest/java/advinha/abctech/com/adivinha/MainActivityTest.java
@@ -0,0 +1,8 @@
+package advinha.abctech.com.adivinha;
+
+/**
+ * Created by regis on 22/10/17.
+ */
+public class MainActivityTest {
+
+}
---
+++ app/src/main/java/advinha/abctech/com/adivinha/MainActivity.java
@@ -0,0 +1,43 @@
+package advinha.abctech.com.adivinha;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView)  findViewById(R.id.resultadoId);
+
+       // textoResultado.setText("texto Alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+
+
+                textoResultado.setText("Numero Escolhido: " + numeroAleatorio);
+
+
+            }
+        });
+
+    }
+}
